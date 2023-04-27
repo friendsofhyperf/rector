3b259ff86a5cf976d9d57bcb038bdf23b38ca1d5
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-04-27 14:04:04 +0800

    ⚙️chore(EasyCodingStandard) update code formatting rules and paths

--- ecs.php
+++ ecs.php
@@ -2,28 +2,18 @@
 
 declare(strict_types=1);
 
-use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
+use PhpCsFixer\Fixer\ReturnNotation\ReturnAssignmentFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
-return function (ECSConfig $ecsConfig): void {
-    $ecsConfig->paths([
-        __DIR__ . '/config',
-    ]);
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->sets([SetList::PSR_12, SetList::SYMPLIFY, SetList::COMMON, SetList::CLEAN_CODE]);
 
-    // this way you add a single rule
-    $ecsConfig->rules([
-        NoUnusedImportsFixer::class,
-    ]);
+    $ecsConfig->paths([__DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/config', __DIR__ . '/ecs.php']);
 
-    // this way you can add sets - group of rules
-    $ecsConfig->sets([
-        // run and fix, one by one
-        // SetList::SPACES,
-        // SetList::ARRAY,
-        // SetList::DOCBLOCK,
-        // SetList::NAMESPACES,
-        // SetList::COMMENTS,
-        // SetList::PSR_12,
+    $ecsConfig->skip([
+        '*/Source/*', '*/Fixture/*',
+        // breaks annotated code - removed on symplify dev-main
+        ReturnAssignmentFixer::class,
     ]);
 };
