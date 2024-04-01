27c067eeddf5aa73b5bd6dab80d6056e08a787e7
Thomas <thomas@sctr.net>
2024-04-01 08:57:57 -0500

    Upgrade to rector 1

---
+++ config/sets/up-to-hyperf31.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Hyperf\Set\HyperfSetList;
+
+return static function (RectorConfig $rectorConfig) : void {
+    $rectorConfig->sets([HyperfSetList::HYPERF_31]);
+};
