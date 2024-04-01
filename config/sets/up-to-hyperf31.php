77820a712d923b83c0d3757e4a9c64a158deb478
Thomas <thomas@sctr.net>
2024-04-01 09:51:20 -0500

    Added ECS back

--- config/sets/up-to-hyperf31.php
+++ config/sets/up-to-hyperf31.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\Hyperf\Set\HyperfSetList;
 
-return static function (RectorConfig $rectorConfig) : void {
+return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->sets([HyperfSetList::HYPERF_31]);
 };
