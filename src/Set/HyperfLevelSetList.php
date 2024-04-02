a574ef92489c62fecfc34564de359f83eba84f6f
Thomas <thomas@sctr.net>
2024-04-01 19:06:41 -0500

    Upgrade to Rector v1 (#2)
    * Allow rector v1
    
    * Upgrade to rector 1
    
    * config
    
    * not needed
    
    * Added ECS back
    
    * Add symplify/phpstan-extensions dependency
    
    * Add symplify/rule-doc-generator to composer.json
    
    ---------
    
    Co-authored-by: Deeka Wong <8337659+huangdijia@users.noreply.github.com>

--- src/Set/HyperfLevelSetList.php
+++ src/Set/HyperfLevelSetList.php
@@ -8,4 +8,5 @@ use Rector\Set\Contract\SetListInterface;
 
 final class HyperfLevelSetList implements SetListInterface
 {
+    public const UP_TO_HYPERF_31 = __DIR__ . '/../../config/sets/up-to-hyperf31.php';
 }
